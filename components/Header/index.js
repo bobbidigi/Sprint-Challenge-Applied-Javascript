b745473ea2be51106ba822b7f31d808e8064aa9f
bobbidigi <bobbidigi34@gmail.com>
2019-08-18 19:28:45 -0400

    add: static header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+  let headerComponent = document.createElement('div');
+  headerComponent.classList.add('header');
+
+  // date
+  let date = document.createElement('span');
+  date.classList.add('date');
+  date.textContent = "MARCH 28, 2019";
+
+  // title
+  let title = document.createElement('h1');
+  title.textContent = "Lambda Times";
+
+  // temp
+  let temp = document.createElement('span');
+  temp.textContent = '98°';
+
+
+  headerComponent.appendChild(date);
+  headerComponent.appendChild(title);
+  headerComponent.appendChild(temp);
+  return headerComponent;
+}
+
+let headerContainer = document.querySelector('.header-container');
+
+headerContainer.appendChild(Header());
